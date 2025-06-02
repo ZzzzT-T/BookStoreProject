7521c0591d53c0fbff31fc05216f3ecfd414bc4c
ZzzzT-T <opps_zhen@yahoo.com>
2025-06-02 20:01:44 +0800

    adding spring boot project details in

--- SpringBootGitSwagggerDockerDemo/src/main/java/com/example/BookStoreDemo/BookStoreApplication.java
+++ SpringBootGitSwagggerDockerDemo/src/main/java/com/example/BookStoreDemo/BookStoreApplication.java
@@ -1,4 +1,4 @@
-package com.example.SpringBootGitSwagggerDockerDemo;
+package com.example.BookStoreDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
